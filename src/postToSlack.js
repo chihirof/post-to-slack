675300152ac95f90e1cc8047e056b25763e0521b
chihiro <fujisawachihiro@gmail.com>
2020-05-20 20:40:53 +0900

    setting eslint and apply

--- src/postToSlack.js
+++ src/postToSlack.js
@@ -1,17 +1,16 @@
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
 
 module.exports = class Slack {
-  constructor(url, message){
-    this.url = url;
-    this.body = {text : message};
+  constructor (url, message) {
+    this.url = url
+    this.body = { text: message }
   }
 
-  post(){
+  post () {
     return fetch(this.url, {
       method: 'post',
-      body:    JSON.stringify(this.body),
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: JSON.stringify(this.body),
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
     }).then(res => res.status)
   }
 }
-
